6f847a92062d3cf528c0ba2ba1ff390334f0fa3f
Mark D Holmberg <mark.d.holmberg@gmail.com>
2022-09-04 15:38:44 -0600

    Custom Item Sheet Template toggles.

--- src/module/ocd5eItemSheet.ts
+++ src/module/ocd5eItemSheet.ts
@@ -7,4 +7,18 @@ export class Ocd5eItemSheet extends ItemSheet5e {
       classes: ['ocd5e', 'dnd5ebak', 'sheet', 'item'],
     });
   }
+
+  /** @inheritdoc */
+  get template() {
+    const path = 'modules/ocd-5e/templates/items/';
+    return `${path}/${this.item.data.type}.html`;
+  }
+
+  /** @inheritdoc */
+  activateListeners(html) {
+    super.activateListeners(html);
+    $("h3.form-header[data-toggle='toggle-header']").click((event) => {
+      $(event.currentTarget).next(".item-section[data-toggle='collapse']").toggle();
+    });
+  }
 }
